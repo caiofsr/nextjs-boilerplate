0755426ecf22c20e94872b1e65e44026fe9122df
César O. Araújo <oaraujocesar@gmail.com>
2021-02-13 16:55:36 -0300

    feat: :lipstick: adding globalStyles and creating absolute path alias

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,22 @@
 import { AppProps } from 'next/app'
+import Head from 'next/head'
 
-const MyApp: React.FC<AppProps> = ({ Component, pageProps }) => (
-  <Component {...pageProps} />
+import GlobalStyles from 'styles/global'
+
+const App: React.FC<AppProps> = ({ Component, pageProps }) => (
+  <>
+    <Head>
+      <title>React Avançado - Boilerplate</title>
+      <link rel="shortcut icon" href="/img/icon-512.png" />
+      <link rel="apple-touch-icon" href="/img/icon-512.png" />
+      <meta
+        name="description"
+        content="A simple starter project to work with TypeScript, React, NextJS and Styled Components"
+      />
+    </Head>
+    <GlobalStyles />
+    <Component {...pageProps} />
+  </>
 )
 
-export default MyApp
+export default App
